5d458dc7ac83441e51d09d1ddc28e04c5fa0c2e6
Kyle Hwang <kylehwang2022@gmail.com>
2021-07-09 13:56:58 -0500

    added some tuff to the api call test

--- login.js
+++ login.js
@@ -1,9 +1,13 @@
 function testAPI() {                      // Testing Graph API after login.  See statusChangeCallback() for when this call is made.
     console.log('Welcome!  Fetching your information.... ');
-    FB.api('/me', function(response) {
-    console.log('Successful login for: ' + response.name);
-    document.getElementById('status').innerHTML =
-        'Thanks for logging in, ' + response.name + '!';
+    FB.api('/me',
+        'GET',
+        {"fields":"id,name,first_name,last_name,friends{hometown, name}"},
+        function(response) {
+        console.log(response);
+        console.log('Successful login for: ' + response.name);
+        document.getElementById('status').innerHTML =
+            'Thanks for logging in, ' + response.name + '!';
     });
 }
 
