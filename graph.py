71d773c20e86ad9cc501483daeeba7c2c00d209a
wildcats27 <briankim2021@u.northwestern.edu>
2021-07-18 23:17:14 -0400

    removed API key

--- graph.py
+++ graph.py
@@ -11,7 +11,7 @@ map_options = GMapOptions(lat=30.2861, lng=-97.7394, map_type="roadmap", zoom=11
 #     https://developers.google.com/maps/documentation/javascript/get-api-key
 #
 # Replace the value below with your personal API key:
-p = gmap("AIzaSyBrz-GHHHwnhQXnNn9949JjCPhvB7CScU4", map_options, plot_width=1000, plot_height=1000)
+p = gmap("", map_options, plot_width=1000, plot_height=1000)
 
 source = ColumnDataSource(
     data=dict(lat=[ 30.29,  30.20,  30.29],
