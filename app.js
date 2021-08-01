6dd7738b2a156f56c5a30f00cb867d2ab3b0e883
Kyle Hwang <kylehwang2022@gmail.com>
2021-08-01 17:16:56 -0500

    changes to how app.js shows pages in a seemingly better way in every way

--- app.js
+++ app.js
@@ -6,13 +6,8 @@ var http = require('http'),
 
 app.use(express.static(path.join(__dirname, '/')));
 
-fs.readFile('./index.html', function (err, html) {
-    if (err) {
-        throw err; 
-    }       
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(process.env.PORT);
+app.listen(process.env.PORT);
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
 });
